317af68f001ab08e76d2eb1be14b724235a9f086
Adona <adonamariasalu2017@gmail.com>
2026-07-24 17:07:52 +0530

    Create task2.js

---
+++ task2.js
@@ -0,0 +1,9 @@
+let systemName = "Hospital Patient System";
+let totalPatients = 150;
+
+console.log("System Name:", systemName);
+console.log("Total Patients:", totalPatients);
+
+// Expected Output:
+// System Name: Hospital Patient System
+// Total Patients: 150
