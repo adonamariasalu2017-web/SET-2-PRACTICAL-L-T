bb8b3dfa14163f032eb6cbbd8273aea416989f82
Adona <adonamariasalu2017@gmail.com>
2026-07-24 17:08:26 +0530

    Initialize patient variables and log their types
    Log the types of patientAge, patientName, and isAdmitted.

---
+++ task3.js
@@ -0,0 +1,12 @@
+let patientAge = 25;
+let patientName = "John";
+let isAdmitted = true;
+
+console.log(typeof patientAge);
+console.log(typeof patientName);
+console.log(typeof isAdmitted);
+
+// Expected Output:
+// number
+// string
+// boolean
